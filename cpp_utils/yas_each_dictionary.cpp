ef2f2fb341101fcc854c352475bf2f91d4166f5d
Yuki Yasoshima <yukiyasos@gmail.com>
2016-01-03 21:03:42 +0900

    each_dictionary

---
+++ cpp_utils/yas_each_dictionary.cpp
@@ -0,0 +1,78 @@
+//
+//  yas_each_dictionary.cpp
+//
+
+#include "yas_each_dictionary.h"
+#include "yas_each_index.h"
+
+using namespace yas;
+
+each_dictionary_iterator::each_dictionary_iterator(cf_type_vector_ptr const &keys_and_values, std::size_t const index)
+    : _keys_and_values(keys_and_values), _index(index) {
+}
+
+each_dictionary_iterator &each_dictionary_iterator::operator++() {
+    ++_index;
+    return *this;
+}
+
+each_dictionary_iterator each_dictionary_iterator::operator++(int) {
+    each_dictionary_iterator result = *this;
+    ++_index;
+    return result;
+}
+
+std::pair<CFTypeRef, CFTypeRef> const &each_dictionary_iterator::operator*() const {
+    if (_index < _keys_and_values->size()) {
+        return _keys_and_values->at(_index);
+    }
+
+    static std::pair<CFTypeRef, CFTypeRef> const null_pair = std::make_pair(nullptr, nullptr);
+    return null_pair;
+}
+
+bool each_dictionary_iterator::operator==(const each_dictionary_iterator &rhs) const {
+    return _index == rhs._index;
+}
+
+bool each_dictionary_iterator::operator!=(const each_dictionary_iterator &rhs) const {
+    return _index != rhs._index;
+}
+
+#pragma mark -
+
+each_dictionary::each_dictionary(CFDictionaryRef const dict)
+    : _keys_and_values(std::make_shared<cf_type_vector>()), _dict(dict) {
+    CFRetain(dict);
+
+    std::size_t const count = CFDictionaryGetCount(dict);
+    _keys_and_values->reserve(count);
+    std::vector<CFTypeRef> keys{count};
+    std::vector<CFTypeRef> values{count};
+
+    CFDictionaryGetKeysAndValues(dict, keys.data(), values.data());
+
+    for (auto &idx : each_index<std::size_t>{count}) {
+        _keys_and_values->emplace_back(std::make_pair(keys.at(idx), values.at(idx)));
+    }
+}
+
+each_dictionary::~each_dictionary() {
+    CFRelease(_dict);
+}
+
+bool each_dictionary::operator==(const each_dictionary &rhs) const {
+    return _dict == rhs._dict;
+}
+
+bool each_dictionary::operator!=(const each_dictionary &rhs) const {
+    return _dict != rhs._dict;
+}
+
+each_dictionary::iterator each_dictionary::begin() const {
+    return iterator{_keys_and_values, 0};
+}
+
+each_dictionary::iterator each_dictionary::end() const {
+    return iterator{_keys_and_values, _keys_and_values->size()};
+}
---
+++ cpp_utils/yas_each_dictionary.h
@@ -0,0 +1,51 @@
+//
+//  yas_each_dictionary.h
+//
+
+#pragma once
+
+#include <CoreFoundation/CoreFoundation.h>
+#include <iterator>
+#include <utility>
+#include <vector>
+
+namespace yas {
+using cf_type_pair = std::pair<CFTypeRef, CFTypeRef>;
+using cf_type_vector = std::vector<cf_type_pair>;
+using cf_type_vector_ptr = std::shared_ptr<cf_type_vector>;
+
+class each_dictionary_iterator : public std::iterator<std::input_iterator_tag, std::pair<CFTypeRef, CFTypeRef>> {
+   public:
+    each_dictionary_iterator(cf_type_vector_ptr const &keys_and_values, std::size_t const index);
+
+    each_dictionary_iterator &operator++();
+    each_dictionary_iterator operator++(int);
+
+    std::pair<CFTypeRef, CFTypeRef> const &operator*() const;
+
+    bool operator==(const each_dictionary_iterator &rhs) const;
+    bool operator!=(const each_dictionary_iterator &rhs) const;
+
+   private:
+    cf_type_vector_ptr _keys_and_values;
+    std::size_t _index;
+};
+
+class each_dictionary {
+   public:
+    using iterator = each_dictionary_iterator;
+
+    each_dictionary(CFDictionaryRef const dict);
+    ~each_dictionary();
+
+    bool operator==(const each_dictionary &rhs) const;
+    bool operator!=(const each_dictionary &rhs) const;
+
+    each_dictionary::iterator begin() const;
+    each_dictionary::iterator end() const;
+
+   private:
+    cf_type_vector_ptr _keys_and_values;
+    CFDictionaryRef _dict;
+};
+}
