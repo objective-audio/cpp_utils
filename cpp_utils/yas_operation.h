96c9cce667cbc8dd891af4f377bbc1a33cbaa3c9
Yuki Yasoshima <yukiyasos@gmail.com>
2016-02-27 14:01:15 +0900

    operation protocol

--- cpp_utils/yas_operation.h
+++ cpp_utils/yas_operation.h
@@ -8,35 +8,25 @@
 #include <functional>
 #include <memory>
 #include "yas_base.h"
+#include "yas_operation_protocol.h"
 
 namespace yas {
 class operation;
-
-class operation_controllable {
-   public:
-    virtual void _execute() = 0;
-    virtual void _cancel() = 0;
-};
 
 class operation : public base, public operation_controllable {
     using super_class = base;
 
    public:
     using execution_f = std::function<void(operation const &)>;
-    using priority_t = UInt32;
 
-    struct option_t {
-        priority_t priority;
-    };
-
-    explicit operation(execution_f const &, option_t opt = {.priority = 0});
-    explicit operation(execution_f &&, option_t opt = {.priority = 0});
+    explicit operation(execution_f const &, operation_option_t opt = {.priority = 0});
+    explicit operation(execution_f &&, operation_option_t opt = {.priority = 0});
     operation(std::nullptr_t);
 
     void cancel();
     bool is_canceled() const;
 
-    option_t const &option() const;
+    operation_option_t const &option() const;
 
    private:
     class impl;
