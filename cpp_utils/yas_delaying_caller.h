17c3df7dea5f5a8a227d7b494689f1d73d1835d5
Yuki Yasoshima <yukiyasos@gmail.com>
2016-08-20 13:12:00 +0900

    delaying caller

---
+++ cpp_utils/yas_delaying_caller.h
@@ -0,0 +1,25 @@
+//
+//  yas_delaying_caller.h
+//
+
+#pragma once
+
+#include <functional>
+
+namespace yas {
+class delaying_caller {
+   public:
+    using handler_f = std::function<void(void)>;
+
+    delaying_caller();
+
+    void request(handler_f);
+
+    void push();
+    void pop();
+
+   private:
+    handler_f _handler;
+    std::size_t _push_count;
+};
+}
