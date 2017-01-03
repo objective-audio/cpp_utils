94fbf24e425202c66ca13fb2bfb66f925816b2fb
Yuki Yasoshima <yukiyasos@gmail.com>
2017-01-03 18:42:39 +0900

    add tst

--- cpp_utils_test/cpp_utils_test/yas_fast_each_tests.mm
+++ cpp_utils_test/cpp_utils_test/yas_fast_each_tests.mm
@@ -4,6 +4,7 @@
 
 #import <XCTest/XCTest.h>
 #import "yas_fast_each.h"
+#import <array>
 
 using namespace yas;
 
@@ -21,7 +22,9 @@ using namespace yas;
     [super tearDown];
 }
 
-- (void)test_create_with_end {
+#pragma mark -
+
+- (void)test_create_fast_each_index_with_end {
     auto each = fast_each<int16_t>{4};
 
     XCTAssertEqual(each._index, 0);
@@ -29,7 +32,7 @@ using namespace yas;
     XCTAssertEqual(each._end, 4);
 }
 
-- (void)test_create_with_range {
+- (void)test_create_fast_each_index_with_range {
     auto each = fast_each<int16_t>{1, 3};
 
     XCTAssertEqual(each._index, 1);
@@ -37,21 +40,40 @@ using namespace yas;
     XCTAssertEqual(each._end, 3);
 }
 
-- (void)test_next {
+- (void)test_make_fast_each_index_with_end {
+    int32_t end = 3;;
+    auto each = make_fast_each(end);
+    
+    XCTAssertEqual(each._index, 0);
+    XCTAssertEqual(each._next, 0);
+    XCTAssertEqual(each._end, 3);
+}
+
+- (void)test_make_fast_each_index_with_range {
+    int32_t start = 1;
+    int32_t end = 3;
+    auto each = make_fast_each(start, end);
+    
+    XCTAssertEqual(each._index, 1);
+    XCTAssertEqual(each._next, 1);
+    XCTAssertEqual(each._end, 3);
+}
+
+- (void)test_fast_each_index_next {
     auto each = fast_each<int16_t>{5, 7};
-    
+
     XCTAssertTrue(yas_fast_each_next(each));
-    
+
     XCTAssertEqual(yas_fast_each_index(each), 5);
-    
+
     XCTAssertTrue(yas_fast_each_next(each));
-    
+
     XCTAssertEqual(yas_fast_each_index(each), 6);
-    
+
     XCTAssertFalse(yas_fast_each_next(each));
 }
 
-- (void)test_stop {
+- (void)test_fast_each_index_stop {
     auto each = fast_each<int16_t>{10};
 
     auto count = 0;
@@ -65,6 +87,32 @@ using namespace yas;
     }
 
     XCTAssertEqual(count, 6);
+}
+
+#pragma mark -
+
+- (void)test_create_fast_each_pointer {
+    std::array<int16_t, 2> array{0, 1};
+    auto each = fast_each<int16_t *>{array.data(), array.size()};
+    
+    XCTAssertEqual(each._ptr, array.data());
+    XCTAssertEqual(each._top_ptr, array.data());
+    XCTAssertTrue(each._ptr_ptr != nullptr);
+    XCTAssertEqual(*each._ptr_ptr, array.data());
+    XCTAssertEqual(each._index, 0);
+    XCTAssertEqual(each.length, 2);
+}
+
+- (void)test_make_fast_each_pointer {
+    std::array<int16_t, 2> array{0, 1};
+    auto each = make_fast_each(array.data(), array.size());
+    
+    XCTAssertEqual(each._ptr, array.data());
+    XCTAssertEqual(each._top_ptr, array.data());
+    XCTAssertTrue(each._ptr_ptr != nullptr);
+    XCTAssertEqual(*each._ptr_ptr, array.data());
+    XCTAssertEqual(each._index, 0);
+    XCTAssertEqual(each.length, 2);
 }
 
 @end
