3d52b6ac59f4ea83896840967ff97fb134270f30
Yuki Yasoshima <yukiyasos@gmail.com>
2017-02-04 13:05:23 +0900

    add compare tests

--- cpp_utils_test/cpp_utils_test/yas_boolean_tests.mm
+++ cpp_utils_test/cpp_utils_test/yas_boolean_tests.mm
@@ -30,17 +30,17 @@ using namespace yas;
 
 - (void)test_copy_constructor {
     boolean true_flag{true};
-    
+
     boolean flag{true_flag};
-    
+
     XCTAssertTrue(flag);
 }
 
 - (void)test_move_constructor {
     boolean true_flag{true};
-    
+
     boolean flag{std::move(true_flag)};
-    
+
     XCTAssertTrue(flag);
 }
 
@@ -59,18 +59,18 @@ using namespace yas;
 - (void)test_copy_assign {
     boolean flag{false};
     boolean true_flag{true};
-    
+
     flag = true_flag;
-    
+
     XCTAssertTrue(flag);
 }
 
 - (void)test_move_assign {
     boolean flag{false};
     boolean true_flag{true};
-    
+
     flag = std::move(true_flag);
-    
+
     XCTAssertTrue(flag);
 }
 
@@ -94,14 +94,74 @@ using namespace yas;
     boolean true_flag2 = true;
     boolean false_flag1 = false;
     boolean false_flag2 = false;
-    
+
     XCTAssertFalse(true_flag1 != true_flag1);
     XCTAssertFalse(true_flag1 != true_flag2);
     XCTAssertFalse(false_flag1 != false_flag1);
     XCTAssertFalse(false_flag1 != false_flag2);
-    
+
     XCTAssertTrue(true_flag1 != false_flag1);
     XCTAssertTrue(false_flag1 != true_flag1);
+}
+
+- (void)test_greater {
+    boolean true_flag1 = true;
+    boolean true_flag2 = true;
+    boolean false_flag1 = false;
+    boolean false_flag2 = false;
+
+    XCTAssertFalse(true_flag1 > true_flag1);
+    XCTAssertFalse(true_flag1 > true_flag2);
+    XCTAssertFalse(false_flag1 > false_flag1);
+    XCTAssertFalse(false_flag1 > false_flag2);
+
+    XCTAssertTrue(true_flag1 > false_flag1);
+    XCTAssertFalse(false_flag1 > true_flag1);
+}
+
+- (void)test_greater_equal {
+    boolean true_flag1 = true;
+    boolean true_flag2 = true;
+    boolean false_flag1 = false;
+    boolean false_flag2 = false;
+
+    XCTAssertTrue(true_flag1 >= true_flag1);
+    XCTAssertTrue(true_flag1 >= true_flag2);
+    XCTAssertTrue(false_flag1 >= false_flag1);
+    XCTAssertTrue(false_flag1 >= false_flag2);
+
+    XCTAssertTrue(true_flag1 >= false_flag1);
+    XCTAssertFalse(false_flag1 >= true_flag1);
+}
+
+- (void)test_less {
+    boolean true_flag1 = true;
+    boolean true_flag2 = true;
+    boolean false_flag1 = false;
+    boolean false_flag2 = false;
+
+    XCTAssertFalse(true_flag1 < true_flag1);
+    XCTAssertFalse(true_flag1 < true_flag2);
+    XCTAssertFalse(false_flag1 < false_flag1);
+    XCTAssertFalse(false_flag1 < false_flag2);
+
+    XCTAssertFalse(true_flag1 < false_flag1);
+    XCTAssertTrue(false_flag1 < true_flag1);
+}
+
+- (void)test_less_equal {
+    boolean true_flag1 = true;
+    boolean true_flag2 = true;
+    boolean false_flag1 = false;
+    boolean false_flag2 = false;
+
+    XCTAssertTrue(true_flag1 <= true_flag1);
+    XCTAssertTrue(true_flag1 <= true_flag2);
+    XCTAssertTrue(false_flag1 <= false_flag1);
+    XCTAssertTrue(false_flag1 <= false_flag2);
+
+    XCTAssertFalse(true_flag1 <= false_flag1);
+    XCTAssertTrue(false_flag1 <= true_flag1);
 }
 
 @end
