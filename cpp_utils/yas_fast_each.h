aace3a1abf39dded7e3f2602e7c56ff6bc981ea9
Yuki Yasoshima <yukiyasos@gmail.com>
2017-04-24 20:15:43 +0900

    pointer access macro

--- cpp_utils/yas_fast_each.h
+++ cpp_utils/yas_fast_each.h
@@ -47,5 +47,6 @@ fast_each<T> make_fast_each(T ptr, std::size_t const end);
 #define yas_each_index(__v) (__v)._index
 #define yas_each_next(__v) (((__v)._index = (__v)._next++) < (__v)._end)
 #define yas_each_value(__v) ((__v)._ptr[(__v)._index])
+#define yas_each_ptr(__v) (&yas_each_value(__v))
 
 #include "yas_fast_each_private.h"
