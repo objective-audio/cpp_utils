4b6d27391ce2e2f837816e64f6649014e6024cd9
Yuki Yasoshima <yukiyasos@gmail.com>
2018-01-04 16:19:03 +0900

    template parameter

--- cpp_utils/yas_state_machine.h
+++ cpp_utils/yas_state_machine.h
@@ -10,7 +10,7 @@
 #include <string>
 
 namespace yas {
-template <typename S = std::string, typename M = std::string, typename R = std::nullptr_t>
+template <typename State = std::string, typename Method = std::string, typename Return = std::nullptr_t>
 class state_machine : public base {
     struct impl;
 
@@ -18,27 +18,29 @@ class state_machine : public base {
     struct changer {
         weak<state_machine> weak_machine;
 
-        void change(S const &) const;
+        void change(State const &) const;
 
-        S const &current() const;
+        State const &current() const;
     };
 
     using entered_handler_f = std::function<void(changer const &)>;
-    using entered_handlers_t = std::unordered_map<S, typename state_machine<S, M, R>::entered_handler_f>;
+    using entered_handlers_t =
+        std::unordered_map<State, typename state_machine<State, Method, Return>::entered_handler_f>;
 
     state_machine();
-    state_machine(S initial_state, entered_handlers_t handlers);
+    state_machine(State initial, entered_handlers_t handlers);
     state_machine(std::nullptr_t);
 
-    void register_state(S const &state, entered_handler_f handler);
+    void register_state(State const &state, entered_handler_f handler);
 
-    void change_state(S const &state);
+    void change_state(State const &state);
 
-    S const &current_state() const;
+    State const &current_state() const;
 };
 
-template <typename S, typename M, typename R = std::nullptr_t>
-state_machine<S, M, R> make_state_machine(S initial, typename state_machine<S, M, R>::entered_handlers_t handlers);
+template <typename State, typename Method, typename Return = std::nullptr_t>
+state_machine<State, Method, Return> make_state_machine(
+    State initial, typename state_machine<State, Method, Return>::entered_handlers_t handlers);
 }
 
 #include "yas_state_machine_private.h"
--- cpp_utils/yas_state_machine_private.h
+++ cpp_utils/yas_state_machine_private.h
@@ -5,15 +5,15 @@
 #pragma once
 
 namespace yas {
-template <typename S, typename M, typename R>
-void state_machine<S, M, R>::changer::change(S const &key) const {
+template <typename State, typename Method, typename Return>
+void state_machine<State, Method, Return>::changer::change(State const &key) const {
     if (auto machine = weak_machine.lock()) {
         machine.change_state(key);
     }
 }
 
-template <typename S, typename M, typename R>
-S const &state_machine<S, M, R>::changer::current() const {
+template <typename State, typename Method, typename Return>
+State const &state_machine<State, Method, Return>::changer::current() const {
     if (auto machine = weak_machine.lock()) {
         return machine.current_state();
     }
@@ -21,17 +21,17 @@ S const &state_machine<S, M, R>::changer::current() const {
     throw std::runtime_error("state_machine lock failed.");
 }
 
-template <typename S, typename M, typename R>
-struct state_machine<S, M, R>::impl : base::impl {
-    std::unordered_map<S, entered_handler_f> handlers;
+template <typename State, typename Method, typename Return>
+struct state_machine<State, Method, Return>::impl : base::impl {
+    std::unordered_map<State, entered_handler_f> handlers;
     changer changer;
-    S current;
+    State current;
 
     void prepare(state_machine &machine) {
         this->changer.weak_machine = to_weak(machine);
     }
 
-    void register_state(S const &key, entered_handler_f &&handler) {
+    void register_state(State const &key, entered_handler_f &&handler) {
         if (this->handlers.count(key) > 0) {
             throw std::invalid_argument("key is already exists.");
         }
@@ -39,7 +39,7 @@ struct state_machine<S, M, R>::impl : base::impl {
         this->handlers.emplace(key, std::move(handler));
     }
 
-    void change_state(S const &state) {
+    void change_state(State const &state) {
         auto &handlers = this->handlers;
         if (handlers.count(state) > 0) {
             this->current = state;
@@ -50,13 +50,14 @@ struct state_machine<S, M, R>::impl : base::impl {
     }
 };
 
-template <typename S, typename M, typename R>
-state_machine<S, M, R>::state_machine() : base(std::make_shared<impl>()) {
+template <typename State, typename Method, typename Return>
+state_machine<State, Method, Return>::state_machine() : base(std::make_shared<impl>()) {
     impl_ptr<impl>()->prepare(*this);
 }
 
-template <typename S, typename M, typename R>
-state_machine<S, M, R>::state_machine(S initial, std::unordered_map<S, entered_handler_f> handlers)
+template <typename State, typename Method, typename Return>
+state_machine<State, Method, Return>::state_machine(State initial,
+                                                    std::unordered_map<State, entered_handler_f> handlers)
     : base(std::make_shared<impl>()) {
     auto imp = impl_ptr<impl>();
     imp->prepare(*this);
@@ -64,27 +65,28 @@ state_machine<S, M, R>::state_machine(S initial, std::unordered_map<S, entered_h
     imp->change_state(std::move(initial));
 }
 
-template <typename S, typename M, typename R>
-state_machine<S, M, R>::state_machine(std::nullptr_t) : base(nullptr) {
+template <typename State, typename Method, typename Return>
+state_machine<State, Method, Return>::state_machine(std::nullptr_t) : base(nullptr) {
 }
 
-template <typename S, typename M, typename R>
-void state_machine<S, M, R>::register_state(S const &state, entered_handler_f handler) {
+template <typename State, typename Method, typename Return>
+void state_machine<State, Method, Return>::register_state(State const &state, entered_handler_f handler) {
     impl_ptr<impl>()->register_state(state, std::move(handler));
 }
 
-template <typename S, typename M, typename R>
-void state_machine<S, M, R>::change_state(S const &state) {
+template <typename State, typename Method, typename Return>
+void state_machine<State, Method, Return>::change_state(State const &state) {
     impl_ptr<impl>()->change_state(state);
 }
 
-template <typename S, typename M, typename R>
-S const &state_machine<S, M, R>::current_state() const {
+template <typename State, typename Method, typename Return>
+State const &state_machine<State, Method, Return>::current_state() const {
     return impl_ptr<impl>()->current;
 }
 
-template <typename S, typename M, typename R>
-state_machine<S, M, R> make_state_machine(S initial, typename state_machine<S, M, R>::entered_handlers_t handlers) {
-    return state_machine<S, M, R>{std::move(initial), std::move(handlers)};
+template <typename State, typename Method, typename Return>
+state_machine<State, Method, Return> make_state_machine(
+    State initial, typename state_machine<State, Method, Return>::entered_handlers_t handlers) {
+    return state_machine<State, Method, Return>{std::move(initial), std::move(handlers)};
 }
 }
