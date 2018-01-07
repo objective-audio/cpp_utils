1b2b31029fcf1ceca45e4df76c0233bcde2e1464
Yuki Yasoshima <yukiyasos@gmail.com>
2018-01-07 13:23:38 +0900

    state_machine

--- cpp_utils/yas_state_machine.h
+++ cpp_utils/yas_state_machine.h
@@ -8,37 +8,41 @@
 #include <functional>
 #include <unordered_map>
 #include <string>
+#include "yas_any.h"
 
 namespace yas {
-template <typename State = std::string, typename Method = std::string, typename Return = std::nullptr_t>
+template <typename State = std::string, typename Method = std::string>
 class state_machine : public base {
     struct impl;
 
    public:
-    struct changer {
-        weak<state_machine> weak_machine;
+    struct context {
+        context(state_machine, any const &);
+
+        any const value;
 
         void change(State const &) const;
-
         State const &current() const;
+
+       private:
+        weak<state_machine> _weak_machine;
     };
 
-    using entered_handler_f = std::function<void(changer const &)>;
-    using entered_handlers_t =
-        std::unordered_map<State, typename state_machine<State, Method, Return>::entered_handler_f>;
-    using method_handler_f = std::function<void(changer const &)>;
-    using returned_handler_f = std::function<Return(void)>;
+    using entered_handler_f = std::function<void(context const &)>;
+    using method_handler_f = std::function<any(context const &)>;
+    using unreturned_handler_f = std::function<void(context const &)>;
 
     state_machine();
     state_machine(std::nullptr_t);
 
-    void register_state(State const &, entered_handler_f);
-    void register_method(State const &state, Method const &, method_handler_f);
-    void register_returned_method(State const &state, Method const &, returned_handler_f);
+    void register_entered(State const &, entered_handler_f);
+    void register_returned(State const &, Method const &, method_handler_f);
+    void register_unreturned(State const &, Method const &, unreturned_handler_f);
 
-    void change(State const &state);
-    void perform(Method const &method);
-    Return perform_returned(Method const &method);
+    void change(State const &);
+    void change(State const &, any const &);
+    any perform(Method const &);
+    any perform(Method const &, any const &);
 
     State const &current_state() const;
 };
