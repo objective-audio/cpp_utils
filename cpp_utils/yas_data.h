92f55e218f3361f302f74fddb2dd986ae0084bae
Yuki Yasoshima <yukiyasos@gmail.com>
2018-03-25 09:50:07 +0900

    update data

--- cpp_utils/yas_data.h
+++ cpp_utils/yas_data.h
@@ -4,42 +4,61 @@
 
 #pragma once
 
+#include "yas_result.h"
 #include <vector>
 
 namespace yas {
 template <typename T>
 struct data {
     T *const ptr;
-    std::size_t const length;
+    std::size_t const length = 0;
+    std::size_t const stride = 1;
 };
 
 template <typename T>
 struct const_data {
     T const *const ptr;
-    std::size_t const length;
+    std::size_t const length = 0;
+    std::size_t const stride = 1;
 };
 
 template <typename T>
-data<T> make_data(T *ptr, std::size_t length);
+data<T> make_data(T *const ptr, std::size_t const length);
+template <typename T>
+data<T> make_data(T *const ptr, std::size_t const length, std::size_t const stride);
 
 template <typename T>
 data<T> make_data(std::vector<T> &);
+template <typename T>
+data<T> make_data(std::vector<T> &, std::size_t const stride);
 
 template <typename T>
-const_data<T> make_const_data(T const *ptr, std::size_t length);
+const_data<T> make_const_data(T const *const ptr, std::size_t const length);
+template <typename T>
+const_data<T> make_const_data(T const *const ptr, std::size_t const length, std::size_t const stride);
 
 template <typename T>
 const_data<T> make_const_data(std::vector<T> const &);
+template <typename T>
+const_data<T> make_const_data(std::vector<T> const &, std::size_t const stride);
 
 template <typename T>
 struct data_copy {
+    enum class error {
+        invalid_data,
+        out_of_range,
+    };
+
+    using result_t = result<std::nullptr_t, error>;
+
     const_data<T> src_data;
     data<T> dst_data;
     std::size_t src_begin_idx = 0;
     std::size_t dst_begin_idx = 0;
     std::size_t length;
 
-    void copy();
+    result_t execute();
+    result_t execute_cyclical();
 };
 }
 
