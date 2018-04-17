d49fd55494bc422907db5d3e64cf78f3b8da9cb4
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-17 09:42:43 +0900

    add flow_graph_tests

---
+++ cpp_utils_test/cpp_utils_test/yas_flow_graph_tests.mm
@@ -0,0 +1,21 @@
+//
+//  yas_flow_graph_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+
+@interface yas_flow_graph_tests : XCTestCase
+
+@end
+
+@implementation yas_flow_graph_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
