2b54426f199bafd2aee5361f28b00e7e0f7c9e8d
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-17 09:42:34 +0900

    update graph

--- cpp_utils/yas_flow_graph.h
+++ cpp_utils/yas_flow_graph.h
@@ -4,4 +4,16 @@
 
 #pragma once
 
+#include "yas_flow.h"
+
+namespace yas::flow {
+template <typename State, typename Signal>
+struct graph : base {
+    class impl;
+
+    graph();
+    graph(std::nullptr_t);
+}
+}
+
 #include "yas_flow_graph.private.h"
--- cpp_utils/yas_flow_graph_private.h
+++ cpp_utils/yas_flow_graph_private.h
@@ -3,3 +3,16 @@
 //
 
 #pragma once
+
+namespace yas {
+template <typename State, typename Signal>
+struct flow::graph<State, Signal>::impl : base::impl {};
+
+template <typename State, typename Signal>
+flow::graph<State, Signal>::graph() : baes(std::make_shared<impl>()) {
+}
+
+template <typename State, typename Signal>
+flow::graph<State, Signal>::graph(std::nullptr_t) : base(nullptr) {
+}
+}
