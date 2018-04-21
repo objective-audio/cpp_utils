c43ad3740c97926b84fa9a6f99753b1a601fb290
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-21 14:49:19 +0900

    add opt_t

--- cpp_utils/yas_types.h
+++ cpp_utils/yas_types.h
@@ -7,5 +7,8 @@
 #include <experimental/optional>
 
 namespace yas {
+template <typename T>
+using opt_t = std::experimental::optional<T>;
+
 constexpr std::experimental::nullopt_t nullopt = std::experimental::nullopt;
 }
