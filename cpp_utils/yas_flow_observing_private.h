b260d20a1a2c7a3ec19cbf9320505c1adbdce402
Yuki Yasoshima <yukiyasos@gmail.com>
2018-04-28 22:27:54 +0900

    rename sender to input

--- cpp_utils/yas_flow_observing_private.h
+++ cpp_utils/yas_flow_observing_private.h
@@ -7,16 +7,16 @@
 namespace yas::flow {
 template <typename Key, typename T>
 flow::node<T, T, T> begin(subject<Key, T> &subject, Key const &key) {
-    flow::input<T> sender;
+    flow::input<T> input;
 
-    auto observer = subject.make_value_observer(key, [weak_sender = to_weak(sender)](T const &value) mutable {
+    auto observer = subject.make_value_observer(key, [weak_sender = to_weak(input)](T const &value) mutable {
         if (auto sender = weak_sender.lock()) {
             sender.send_value(value);
         }
     });
 
-    sender.set_can_send_handler([observer]() { return false; });
+    input.set_can_send_handler([observer]() { return false; });
 
-    return sender.begin();
+    return input.begin();
 }
 }
