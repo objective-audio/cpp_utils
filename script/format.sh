1b1695700b1aaa6285bf9de118e7d6103be55fdd
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-13 20:18:35 +0900

    update format.sh

--- script/format.sh
+++ script/format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-clang-format -i -style=file `find cpp_utils cpp_utils_test/cpp_utils_test -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
+clang-format -i -style=file `find ../cpp_utils ../cpp_utils_test/cpp_utils_test -type f \( -name *.h -o -name *.cpp -o -name *.hpp -o -name *.m -o -name *.mm \)`
