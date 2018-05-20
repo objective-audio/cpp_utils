62a4ca7bead6ac7bbc12424f475676d7d609bb3d
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-20 12:01:51 +0900

    enable_if_t

--- cpp_utils/yas_type_traits.h
+++ cpp_utils/yas_type_traits.h
@@ -18,15 +18,15 @@ class has_operator_bool {
     static bool constexpr value = decltype(confirm(std::declval<T>()))::value;
 };
 
-template <typename T>
-using enable_if_integral_t = typename std::enable_if_t<std::is_integral<T>::value>;
-template <typename T>
-using enable_if_pointer_t = typename std::enable_if_t<std::is_pointer<T>::value>;
+template <typename T, typename U = void>
+using enable_if_integral_t = typename std::enable_if_t<std::is_integral<T>::value, U>;
+template <typename T, typename U = void>
+using enable_if_pointer_t = typename std::enable_if_t<std::is_pointer<T>::value, U>;
 
 template <typename>
 struct is_tuple : std::false_type {};
 template <typename... T>
 struct is_tuple<std::tuple<T...>> : std::true_type {};
-template <typename T>
-using enable_if_tuple_t = typename std::enable_if_t<is_tuple<T>::value>;
+template <typename T, typename U = void>
+using enable_if_tuple_t = typename std::enable_if_t<is_tuple<T>::value, U>;
 }  // namespace yas
