c01a87124fa0a57942354db31a385bbea899cf90
Yuki Yasoshima <yukiyasos@gmail.com>
2018-05-20 12:29:31 +0900

    disable_if_tuple_t

--- cpp_utils/yas_type_traits.h
+++ cpp_utils/yas_type_traits.h
@@ -29,4 +29,6 @@ template <typename... T>
 struct is_tuple<std::tuple<T...>> : std::true_type {};
 template <typename T, typename U = void>
 using enable_if_tuple_t = typename std::enable_if_t<is_tuple<T>::value, U>;
+template <typename T, typename U = void>
+using disable_if_tuple_t = typename std::enable_if_t<!is_tuple<T>::value, U>;
 }  // namespace yas
