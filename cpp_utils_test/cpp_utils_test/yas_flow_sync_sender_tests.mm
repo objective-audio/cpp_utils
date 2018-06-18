01dd777601397fdce077e3f90d23372568234773
Yuki Yasoshima <yukiyasos@gmail.com>
2018-06-18 22:50:11 +0900

    add test

---
+++ cpp_utils_test/cpp_utils_test/yas_flow_sync_sender_tests.mm
@@ -0,0 +1,67 @@
+//
+//  yas_flow_sync_sender_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_flow.h"
+
+using namespace yas;
+
+@interface yas_flow_sync_sender_tests : XCTestCase
+
+@end
+
+@implementation yas_flow_sync_sender_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_sync {
+    flow::sync_sender<int> sender;
+
+    int sending = 1;
+
+    sender.set_sync_handler([&sending] { return sending; });
+
+    int notified = -1;
+
+    auto flow = sender.begin_flow().perform([&notified](int const &value) { notified = value; }).sync();
+
+    XCTAssertEqual(notified, 1);
+
+    sending = 2;
+
+    sender.sync();
+
+    XCTAssertEqual(notified, 2);
+}
+
+- (void)test_receive {
+    flow::sync_sender<int> synchronizer;
+    flow::sender<std::nullptr_t> sender;
+
+    int sending = 1;
+
+    synchronizer.set_sync_handler([&sending] { return sending; });
+
+    int notified = -1;
+
+    auto flow = synchronizer.begin_flow().perform([&notified](int const &value) { notified = value; }).sync();
+
+    XCTAssertEqual(notified, 1);
+
+    sending = 2;
+
+    auto receive_flow = sender.begin_flow().receive(synchronizer.receiver()).end();
+
+    sender.send_value(nullptr);
+
+    XCTAssertEqual(notified, 2);
+}
+
+@end
