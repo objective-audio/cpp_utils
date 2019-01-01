d46f1d15db236c248e7daba1dd710da4234dc834
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-01 20:02:46 +0900

    fix cpp_utils.h

--- cpp_utils_ios/cpp_utils/cpp_utils.h
+++ cpp_utils_ios/cpp_utils/cpp_utils.h
@@ -1,8 +1,5 @@
 //
 //  cpp_utils.h
-//  cpp_utils
-//
-//  Created by 八十嶋祐樹 on 2019/01/01.
 //
 
 #import <UIKit/UIKit.h>
@@ -12,7 +9,3 @@ FOUNDATION_EXPORT double cpp_utilsVersionNumber;
 
 //! Project version string for cpp_utils.
 FOUNDATION_EXPORT const unsigned char cpp_utilsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <cpp_utils/PublicHeader.h>
-
-
--- cpp_utils_mac/cpp_utils/cpp_utils.h
+++ cpp_utils_mac/cpp_utils/cpp_utils.h
@@ -9,7 +9,3 @@ FOUNDATION_EXPORT double cpp_utilsVersionNumber;
 
 //! Project version string for cpp_utils.
 FOUNDATION_EXPORT const unsigned char cpp_utilsVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <cpp_utils/PublicHeader.h>
-
-
