9c7f54ee7ee7d98b73a20020c31065d7b50d14e1
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-01 19:03:28 +0900

    add target

---
+++ cpp_utils_ios/cpp_utils/cpp_utils.h
@@ -0,0 +1,18 @@
+//
+//  cpp_utils.h
+//  cpp_utils
+//
+//  Created by 八十嶋祐樹 on 2019/01/01.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for cpp_utils.
+FOUNDATION_EXPORT double cpp_utilsVersionNumber;
+
+//! Project version string for cpp_utils.
+FOUNDATION_EXPORT const unsigned char cpp_utilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cpp_utils/PublicHeader.h>
+
+
---
+++ cpp_utils_mac/cpp_utils/cpp_utils.h
@@ -0,0 +1,15 @@
+//
+//  cpp_utils.h
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for cpp_utils.
+FOUNDATION_EXPORT double cpp_utilsVersionNumber;
+
+//! Project version string for cpp_utils.
+FOUNDATION_EXPORT const unsigned char cpp_utilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <cpp_utils/PublicHeader.h>
+
+
