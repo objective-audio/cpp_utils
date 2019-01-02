124d026ee455895a058cef14e4b1f7379924eaa9
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-02 22:35:54 +0900

    umbrella

--- cpp_utils_mac/cpp_utils/cpp_utils.h
+++ cpp_utils_mac/cpp_utils/cpp_utils.h
@@ -9,3 +9,5 @@ FOUNDATION_EXPORT double cpp_utilsVersionNumber;
 
 //! Project version string for cpp_utils.
 FOUNDATION_EXPORT const unsigned char cpp_utilsVersionString[];
+
+#import <cpp_utils/yas_cpp_utils_umbrella.h>
