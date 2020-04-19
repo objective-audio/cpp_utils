a12f345220c986d9f43577f6c618d4ed057f0e72
Yuki Yasoshima <yukiyasos@gmail.com>
2020-04-19 16:15:24 +0900

    add tests

---
+++ cpp_utils_tests/yas_ns_utils_tests.mm
@@ -0,0 +1,21 @@
+//
+//  yas_ns_utils_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <cpp_utils/yas_ns_utils.h>
+
+using namespace yas;
+
+@interface yas_ns_utils_tests : XCTestCase
+
+@end
+
+@implementation yas_ns_utils_tests
+
+- (void)test_to_ns_object_from_string {
+    auto ns_object = to_ns_object("test_string");
+    XCTAssertEqualObjects(ns_object.object(), @"test_string");
+}
+
+@end
