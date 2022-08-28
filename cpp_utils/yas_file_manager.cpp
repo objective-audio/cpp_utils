f703f0fbacb6ffc4f259364da73ea71572184d63
Yuki Yasoshima <yukiyasos@gmail.com>
2022-08-28 18:38:54 +0900

    use filesystem

--- cpp_utils/yas_file_manager.cpp
+++ cpp_utils/yas_file_manager.cpp
@@ -3,37 +3,49 @@
 //
 
 #include "yas_file_manager.h"
-#import <Foundation/Foundation.h>
+
 #include "yas_cf_utils.h"
 
 using namespace yas;
 
 file_manager::create_dir_result_t file_manager::create_directory_if_not_exists(std::string const &path) {
-    auto file_manager = [NSFileManager defaultManager];
-    CFStringRef cf_path = to_cf_object(path);
+    return create_directory_if_not_exists(std::filesystem::path(path));
+}
 
-    if (auto exists_result = file_manager::content_exists(path); exists_result.is_error()) {
-        NSError *error = nil;
-        if (![file_manager createDirectoryAtPath:(__bridge NSString *)cf_path
-                     withIntermediateDirectories:YES
-                                      attributes:nil
-                                           error:&error]) {
-            return create_dir_result_t{create_dir_error::create_failed};
-        }
-    } else if (exists_result.value() == content_kind::file) {
+file_manager::create_dir_result_t file_manager::create_directory_if_not_exists(std::filesystem::path const &path) {
+    std::error_code error_code;
+
+    if (std::filesystem::exists(path, error_code)) {
         return create_dir_result_t{create_dir_error::file_exists};
+    }
+
+    if (!std::filesystem::create_directories(path, error_code)) {
+        return create_dir_result_t{create_dir_error::create_failed};
     }
 
     return create_dir_result_t{nullptr};
 }
 
 file_manager::exists_result_t file_manager::content_exists(std::string const &path) {
-    auto file_manager = [NSFileManager defaultManager];
-    BOOL is_directory = NO;
-    CFStringRef cf_path = to_cf_object(path);
+    return content_exists(std::filesystem::path(path));
+}
 
-    if ([file_manager fileExistsAtPath:(__bridge NSString *)cf_path isDirectory:&is_directory]) {
-        if (is_directory) {
+file_manager::exists_result_t file_manager::content_exists(std::filesystem::path const &content_path) {
+    std::error_code error_code;
+
+    bool exists = std::filesystem::exists(content_path, error_code);
+
+    if (error_code) {
+        return exists_result_t{nullptr};
+    }
+
+    if (exists) {
+        bool const result = std::filesystem::is_directory(content_path, error_code);
+        if (error_code) {
+            return exists_result_t{nullptr};
+        }
+
+        if (result) {
             return exists_result_t{content_kind::directory};
         } else {
             return exists_result_t{content_kind::file};
@@ -44,11 +56,16 @@ file_manager::exists_result_t file_manager::content_exists(std::string const &pa
 }
 
 file_manager::remove_content_t file_manager::remove_content(std::string const &path) {
-    if (file_manager::content_exists(path)) {
-        auto file_manager = [NSFileManager defaultManager];
-        CFStringRef cf_path = to_cf_object(path);
+    return remove_content(std::filesystem::path(path));
+}
 
-        if (![file_manager removeItemAtPath:(__bridge NSString *)cf_path error:nil]) {
+file_manager::remove_content_t file_manager::remove_content(std::filesystem::path const &path) {
+    if (file_manager::content_exists(path)) {
+        std::error_code error_code;
+
+        bool result = std::filesystem::remove_all(path, error_code);
+
+        if (error_code || !result) {
             return remove_content_t{remove_content_error::remove_failed};
         }
     }
@@ -56,6 +73,10 @@ file_manager::remove_content_t file_manager::remove_content(std::string const &p
 }
 
 file_manager::remove_contents_result_t file_manager::remove_contents_in_directory(std::string const &path) {
+    return remove_contents_in_directory(std::filesystem::path{path});
+}
+
+file_manager::remove_contents_result_t file_manager::remove_contents_in_directory(std::filesystem::path const &path) {
     if (auto const result = file_manager::content_exists(path)) {
         if (result.value() != content_kind::directory) {
             return remove_contents_result_t{remove_contents_error::not_directory};
@@ -64,31 +85,29 @@ file_manager::remove_contents_result_t file_manager::remove_contents_in_director
         return remove_contents_result_t{nullptr};
     }
 
-    auto file_manager = [NSFileManager defaultManager];
-    CFStringRef cf_path = to_cf_object(path);
+    std::error_code error_code;
 
-    @autoreleasepool {
-        NSError *error = nil;
-        NSArray<NSURL *> *urls =
-            [file_manager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:(__bridge NSString *)cf_path]
-                        includingPropertiesForKeys:nil
-                                           options:kNilOptions
-                                             error:&error];
-        if (error) {
-            return remove_contents_result_t{remove_contents_error::find_contents_failed};
-        }
+    auto const iterator = std::filesystem::directory_iterator(path, error_code);
 
-        for (NSURL *url in urls) {
-            if (![file_manager removeItemAtURL:url error:nil]) {
-                return remove_contents_result_t{remove_contents_error::remove_failed};
-            }
-        }
-
-        return remove_contents_result_t{nullptr};
+    if (error_code) {
+        return remove_contents_result_t{remove_contents_error::find_contents_failed};
     }
+
+    for (auto const &content : iterator) {
+        auto const result = std::filesystem::remove_all(content.path(), error_code);
+        if (error_code || !result) {
+            return remove_contents_result_t{remove_contents_error::remove_failed};
+        }
+    }
+
+    return remove_contents_result_t{nullptr};
 }
 
 file_manager::content_paths_result_t file_manager::content_paths_in_directory(std::string const &path) {
+    return content_paths_in_directory(std::filesystem::path(path));
+}
+
+file_manager::content_paths_result_t file_manager::content_paths_in_directory(std::filesystem::path const &path) {
     if (auto const result = file_manager::content_exists(path)) {
         if (result.value() != content_kind::directory) {
             return content_paths_result_t{content_paths_error::not_directory};
@@ -97,29 +116,21 @@ file_manager::content_paths_result_t file_manager::content_paths_in_directory(st
         return content_paths_result_t{content_paths_error::directory_not_found};
     }
 
-    auto file_manager = [NSFileManager defaultManager];
-    CFStringRef cf_path = to_cf_object(path);
+    std::error_code error_code;
 
-    @autoreleasepool {
-        NSError *error = nil;
-        NSArray<NSURL *> *urls =
-            [file_manager contentsOfDirectoryAtURL:[NSURL fileURLWithPath:(__bridge NSString *)cf_path]
-                        includingPropertiesForKeys:nil
-                                           options:kNilOptions
-                                             error:&error];
+    auto const iterator = std::filesystem::directory_iterator(path, error_code);
 
-        if (error) {
-            return content_paths_result_t{content_paths_error::find_contents_failed};
-        }
-
-        std::vector<std::string> paths;
-
-        for (NSURL *url in urls) {
-            paths.emplace_back(to_string((__bridge CFStringRef)url.path));
-        }
-
-        return content_paths_result_t{std::move(paths)};
+    if (error_code) {
+        return content_paths_result_t{content_paths_error::find_contents_failed};
     }
+
+    std::vector<std::string> paths;
+
+    for (auto const &content : iterator) {
+        paths.emplace_back(content.path().string());
+    }
+
+    return content_paths_result_t{std::move(paths)};
 }
 
 std::string yas::to_string(file_manager::create_dir_error const &error) {
